c027c612b37dbe87587604fc2cf8a4509436df7d
LysenkoWork <lyss.work@gmail.com>
2022-12-08 14:49:29 +0300

    some update

--- lab13/task_3/main.py
+++ lab13/task_3/main.py
@@ -3,3 +3,12 @@
 текстовый файл либо в файл HTML-формата (тип файла указывается с 
 помощью выпадающего меню)."""
 
+from tkinter import *
+
+window = Tk()
+window.title("Text translator by Maxim Lysenko")
+window.geometry('400x250')
+#lbl = Label(window, text="Привет", font=("Arial Bold", 50))
+#lbl.grid(column=0, row=0)
+
+window.mainloop()
