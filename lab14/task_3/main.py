2729e7e745373374328cd0e73fb88ff0bfbae462
LysenkoWork <lyss.work@gmail.com>
2022-12-09 10:51:42 +0300

    add lab14/task_3

--- lab14/task_3/main.py
+++ lab14/task_3/main.py
@@ -2,13 +2,52 @@
 текст, введенный с клавиатуры, по нажатию кнопки сохранить в обычный 
 текстовый файл либо в файл HTML-формата (тип файла указывается с 
 помощью выпадающего меню)."""
-
 from tkinter import *
+import datetime
 
-window = Tk()
-window.title("Text translator by Maxim Lysenko")
-window.geometry('400x250')
-# lbl = Label(window, text="Привет", font=("Arial Bold", 50))
-# lbl.grid(column=0, row=0)
+now = datetime.datetime.now()
 
-window.mainloop()
+
+def translate_to_html():
+    _name_files = str(now.date()) + "-" + str(now.time())[0:5].replace(":", "-")
+    with open(f"result/{_name_files}.html", "w") as r:
+        html_top = """
+        <html>
+          <head>
+           <meta http-equiv="Content-Type" content="text/html">
+           <title>Пример веб-страницы</title>
+         </head>
+         <body>
+         <h1>"""
+        html_bottom = """
+        </h1>
+        </body>
+        </html>
+        """
+        r.write(html_top + text.get(1.0, END) + html_bottom)
+        print(str(now.date()))
+
+
+def translate_to_txt():
+    _name_files = str(now.date()) + "-" + str(now.time())[0:5].replace(":", "-")
+    with open(f"result/{_name_files}.txt", "w") as r:
+        r.write(text.get(1.0, END))
+        print()
+
+
+root = Tk()
+root.title("Text translator by Maxim Lysenko")
+root.geometry('400x150')
+lbl = Label(text="Введите текст:", font=("Arial Bold", 10))
+lbl.pack()
+text = Text(width=25, height=5)
+text.pack()
+
+frame = Frame()
+frame.pack()
+Button(frame, text="В HTML",
+       command=translate_to_html).pack(side=LEFT)
+Button(frame, text="В TXT",
+       command=translate_to_txt).pack(side=LEFT)
+
+root.mainloop()
